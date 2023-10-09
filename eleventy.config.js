8824b14759629c0dc4973e882dcaa55405680316
Luka Renko <46861689+lukarenko@users.noreply.github.com>
2023-10-09 14:26:24 +0200

    Fix production deploy

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,3 +1,5 @@
+const fs = require("fs")
+const sass = require("sass")
 const path = require("path")
 
 const Image = require("@11ty/eleventy-img")
@@ -27,6 +29,18 @@ async function imageShortcode(src, alt, sizes) {
 }
 
 module.exports = function(eleventyConfig) {
+    eleventyConfig.on('beforeBuild', () => {
+        const result = sass.renderSync({
+            file: "styles/main.sass",
+            outputStyle: "compressed"
+        });
+        const stylesPath = "dist/styles"
+        if (!fs.existsSync(stylesPath)){
+            fs.mkdirSync(stylesPath, { recursive: true });
+        }
+        fs.writeFileSync(path.join(stylesPath, "main.css"), result.css);
+    });
+
     eleventyConfig.addPlugin(VitePlugin, {
         // Any plugin options can go here
     });
