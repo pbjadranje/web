36cd737dc051ec219e8702201354e51c7d407fd0
Luka Renko <46861689+lukarenko@users.noreply.github.com>
2023-11-30 19:33:32 +0100

    Add swipe forecast change

--- code/main.js
+++ code/main.js
@@ -10,9 +10,9 @@ function updateForecasts() {
 
         images.forEach(img => {
             if (img.alt === selectedForecast) {
-                img.removeAttribute('hidden');
+                img.parentElement.removeAttribute('hidden');
             } else {
-                img.setAttribute('hidden', 'true');
+                img.parentElement.setAttribute('hidden', 'true');
             }
         });
     });
@@ -21,5 +21,42 @@ function updateForecasts() {
 forecastSelector.addEventListener('change', updateForecasts);
 
 document.addEventListener('DOMContentLoaded', () => {
+    var startX, endX;
+
+    const allForecasts = document.querySelectorAll('.forecast');
+
+    allForecasts.forEach(fc => {
+        fc.addEventListener('touchstart', function(e) {
+            startX = e.touches[0].clientX;
+        });
+
+        fc.addEventListener('touchend', function(e) {
+            endX = e.changedTouches[0].screenX;
+
+            if (startX > endX + 50) {       // Swipe left (next image)
+                changeImage(1);
+            }
+            else if (startX < endX - 50) {  // Swipe right (previous image)
+                changeImage(-1);
+            }
+        });
+    });
+
+    function changeImage(direction) {
+        var currentSelectedDate = forecastSelector.value;
+        var dates = Array.from(forecastSelector.options).map(option => option.value);
+        var currentIndex = dates.indexOf(currentSelectedDate);
+        var nextIndex;
+
+        if (direction === 1) { // Swipe left (next image)
+            nextIndex = currentIndex + 1 < dates.length ? currentIndex + 1 : 0;
+        } else { // Swipe right (previous image)
+            nextIndex = currentIndex - 1 >= 0 ? currentIndex - 1 : dates.length - 1;
+        }
+
+        forecastSelector.value = dates[nextIndex];
+        updateForecasts();
+    }
+
     updateForecasts();
 });
