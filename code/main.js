58a7d3638a5cad2492ce8b04ae4d6f75ddaec066
Luka Renko <46861689+lukarenko@users.noreply.github.com>
2023-12-04 15:18:41 +0100

    Add prev/next buttons, refac code

--- code/main.js
+++ code/main.js
@@ -1,9 +1,10 @@
 
+var startX, endX;
 const forecastSelector = document.getElementById('forecastSelector');
+const allForecasts = document.querySelectorAll('.forecast');
 
 function updateForecasts() {
     const selectedForecast = forecastSelector.value;
-    const allForecasts = document.querySelectorAll('.forecast');
 
     allForecasts.forEach(fc => {
         const images = fc.querySelectorAll('img');
@@ -18,45 +19,50 @@ function updateForecasts() {
     });
 }
 
-forecastSelector.addEventListener('change', updateForecasts);
+function changeImage(direction) {
+    var currentSelectedDate = forecastSelector.value;
+    var dates = Array.from(forecastSelector.options).map(option => option.value);
+    var currentIndex = dates.indexOf(currentSelectedDate);
+    var nextIndex;
 
-document.addEventListener('DOMContentLoaded', () => {
-    var startX, endX;
-
-    const allForecasts = document.querySelectorAll('.forecast');
-
-    allForecasts.forEach(fc => {
-        fc.addEventListener('touchstart', function(e) {
-            startX = e.touches[0].clientX;
-        });
-
-        fc.addEventListener('touchend', function(e) {
-            endX = e.changedTouches[0].screenX;
-
-            if (startX > endX + 50) {       // Swipe left (next image)
-                changeImage(1);
-            }
-            else if (startX < endX - 50) {  // Swipe right (previous image)
-                changeImage(-1);
-            }
-        });
-    });
-
-    function changeImage(direction) {
-        var currentSelectedDate = forecastSelector.value;
-        var dates = Array.from(forecastSelector.options).map(option => option.value);
-        var currentIndex = dates.indexOf(currentSelectedDate);
-        var nextIndex;
-
-        if (direction === 1) { // Swipe left (next image)
-            nextIndex = currentIndex + 1 < dates.length ? currentIndex + 1 : 0;
-        } else { // Swipe right (previous image)
-            nextIndex = currentIndex - 1 >= 0 ? currentIndex - 1 : dates.length - 1;
-        }
-
-        forecastSelector.value = dates[nextIndex];
-        updateForecasts();
+    if (direction === 1) {
+        nextIndex = currentIndex + 1 < dates.length ? currentIndex + 1 : 0;
+    } else {
+        nextIndex = currentIndex - 1 >= 0 ? currentIndex - 1 : dates.length - 1;
     }
 
+    forecastSelector.value = dates[nextIndex];
+    updateForecasts();
+}
+
+function prevImage() {
+    changeImage (-1);
+}
+
+function nextImage() {
+    changeImage (1);
+}
+
+forecastSelector.addEventListener('change', updateForecasts);
+
+const prevButtons = document.querySelectorAll('.forecast-prev');
+prevButtons.forEach(btn => btn.addEventListener('click', prevImage));
+
+const nextButtons = document.querySelectorAll('.forecast-next');
+nextButtons.forEach(btn => btn.addEventListener('click', nextImage));
+
+allForecasts.forEach(fc => {
+    fc.addEventListener('touchstart', function(e) {
+        startX = e.touches[0].clientX;
+    });
+
+    fc.addEventListener('touchend', function(e) {
+        endX = e.changedTouches[0].screenX;
+        if (startX > endX + 50) nextImage()
+        else if (startX < endX - 50) prevImage();
+    });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
     updateForecasts();
 });
